f7932a71973f1e9ffd5204bc5fae0f1f4d2455ec
bnb4505 <bnb4505@g.rit.edu>
2021-11-15 21:37:56 -0500

    fixed up inputs, added debug refresh button to get data

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
     sports sentiment predictor project. 
 
 # Features
-    Specify two teams, a city that is hosting the game, the date of the game, and the
+    Specify two teams, the respective cities of each teams, the date of the game, and the
     type of sport the teams are both playing. (We dont have much input checking, so
-    please make sure its correct otherwise the output will be bad)
+    please make sure its correct otherwise the output will be bad). e.g if you enter in the
+    wrong city, the search results will be wonky, so please double check!
 
     Two cards at the bottom that show the general sentiment, and the confidence
     of the sentiment.
@@ -22,3 +23,8 @@
                    "totalTweets" : "",
                 }
             ```
+
+# Parent Project
+    The main project this is part of is linked here https://github.com/RIT-cloud-computing/term-project-team-team-3
+
+    The website had to be hosted in a separate public repo, because AWS Amplify requires either a public repo or access to a private repo, and since the parent repo is private, it was unable to get access to the code.
--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const { HttpResponse } = require("aws-sdk");
+
 var g_team1Name = "";
 var g_team2Name = "";
 
@@ -10,15 +12,16 @@ var apiURL = "";
  */
 function confirmParameters(){
     t1n = document.forms[0].team1name.value;
+    city1 = city1 = document.forms[0].city1.value;
     t2n = document.forms[0].team2name.value;
-    city = document.forms[0].city.value;
+    city2 = document.forms[0].city2.value;
     date = document.forms[0].date.value;
     sport = document.forms[0].sport.value;
-    console.log(t1n + " " + t2n + " " + city + " " + date + " " + sport)
-    if(dateValidation(date) && inputVal(t1n) && inputVal(t2n) && inputVal(city)){
+    console.log(t1n + " " + city1 + " " + t2n + " " + city2 + " " + date + " " + sport)
+    if(dateValidation(date) && inputVal(t1n) && inputVal(t2n) && inputVal(city1) && inputVal(city2)){
         g_team1Name = t1n
         g_team2Name = t2n
-        alert(t1n + " \n" + t2n + " \n" + city + " \n" + date + " \n" + sport)
+        alert(t1n + " \n" + city1 + " \n" + t2n + " \n" + city2 + " \n" + date + " \n" + sport)
     }else{
         let alertString = ""
         if(!dateValidation(date)){
@@ -27,8 +30,10 @@ function confirmParameters(){
             alertString = alertString + "Team 1 name missing or format incorrect \n"
         }if(!inputVal(t2n)){
             alertString = alertString + "Team 2 name missing or format incorrect \n"
-        }if(!inputVal(city)){
-            alertString = alertString + "City name missing or format incorrect \n"
+        }if(!inputVal(city1)){
+            alertString = alertString + "1st City name missing or format incorrect \n"
+        }if(!inputVal(city2)){
+            alertString = alertString + "2nd City name missing or format incorrect \n"
         }
         alert(alertString)
     }
@@ -59,14 +64,14 @@ function dateValidation(date){
  */
 
 
-var http = new XMLHttpRequest();
 
 
-
-function postUserInput(team1name, team2name, city, sport, date){
+function postUserInput(team1name, city1, team2name, city2, sport, date){
+    let http = new XMLHttpRequest();
     var params = 'team1=' + team1name
+                + '&city1=' + city1
                 + '&team2=' + team2name
-                + '&city=' + city
+                + '&city2=' + city2
                 + '&sport=' + sport
                 + '&date=' + date
 
@@ -80,6 +85,18 @@ function postUserInput(team1name, team2name, city, sport, date){
     }
     http.send(params);
     
+}
+
+function getFinalResult(){
+    alert("getting results rn")
+    let http = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (http.readyState == 4 && http.status == 200)
+            //callback(xmlHttp.responseText);
+            updateTeamData(http.responseText[0]);
+    }
+    http.open("GET", apiURL, true);
+    http.send(null);
 }
 
 
